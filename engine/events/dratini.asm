29eeab3b93f7f71a50adcd6db46c147e57c696cd
henrxv <20733573+henrxv@users.noreply.github.com>
2018-02-20 14:51:46 -0400

    Twister removal

--- engine/events/dratini.asm
+++ engine/events/dratini.asm
@@ -73,7 +73,7 @@ Special_Dratini: ; 0x8b170
 ; Dratini does not normally learn Extremespeed. This is a special gift.
 	db WRAP
 	db THUNDER_WAVE
-	db TWISTER
+	db DRAGON_RAGE
 	db EXTREMESPEED
 	db 0
 .Moveset1:
@@ -81,7 +81,7 @@ Special_Dratini: ; 0x8b170
 	db WRAP
 	db LEER
 	db THUNDER_WAVE
-	db TWISTER
+	db DRAGON_RAGE
 	db 0
 
 .GetNthPartyMon: ; 0x8b1ce
